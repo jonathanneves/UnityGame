ce3312d791251d66316ac694e9877b4b1c3c41c6
VBViniciusVB <viniciusvianalaguna@hotmail.com>
2020-04-10 15:43:13 -0300

    First Commit
    Commit inicial

---
+++ Pokiball World/Assets/Scripts/Ball_Movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movement : MonoBehaviour
+{
+
+    Rigidbody myRigidbody;
+
+    [SerializeField] float moveSpeed = 0f;
+    [SerializeField] float jumpPower = 0f;
+    float hInput;
+    float vInput;
+    bool jumpInput;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal") * moveSpeed;
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+        if (jumpInput)
+        {
+            Jump();
+            jumpInput = false;
+        }
+
+    }
+
+    void Move()
+    {
+        myRigidbody.AddForce(hInput * Time.fixedDeltaTime, 0f, vInput * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        myRigidbody.AddForce(Vector3.up * jumpPower * Time.fixedDeltaTime, ForceMode.Impulse);
+    }
+
+
+}
---
+++ Pokiball World/Assets/Scripts/Cam_Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam_Movement : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] float followSpeed = 0f;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        offset = target.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.position = Vector3.Lerp(transform.position, target.position - offset, Time.deltaTime * followSpeed);
+
+    }
+}
